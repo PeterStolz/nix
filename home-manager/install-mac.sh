69071de007d676c0b03930f9d944b6e5bc1fecdc
Peter Stolz <50801264+PeterStolz@users.noreply.github.com>
2025-06-06 16:00:18 +0200

    Update install-mac.sh

--- home-manager/install-mac.sh
+++ home-manager/install-mac.sh
@@ -4,7 +4,7 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 nix-shell -p git --command "git clone https://github.com/PeterStolz/nix.git"
 mkdir .config
 ln -s $PWD/nix/home-manager $PWD/.config/home-manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
